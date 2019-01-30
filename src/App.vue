e500c8d95d1dfce47963c7c8f7cf1dbc6b6d2283
somto96 <motivatedsomto@gmail.com>
2019-01-30 15:38:51 +0100

    Refactored code base

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,54 @@
 <template>
   <div id="app">
     <Header />
-    <router-view/>
+    <AddTodo @add-todo='addTodo'/>
+    <Todos v-bind:todos="todos" @del-todo='deleteTodo'/>
   </div>
 </template>
 
 <script>
 import Header from './components/layout/header';
+import Todos from './components/todo';
+import AddTodo from './components/AddTodo';
+import axios from 'axios'
+// import fetch from 'fetch'
 export default {
   name: 'app',
-  components:{
-    Header
+  components: {
+    Header,
+    Todos,
+    AddTodo 
+  },
+  data(){
+    return{
+      todos:[]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res => this.todos = this.todos.filter(todo => todo.id !== id ))
+      .catch(err => console.log(err))
+      
+    },
+    addTodo(newTodo){
+      const {title, completed} = newTodo
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title, 
+        completed
+      })
+      .then(res => this.todos = [...this.todos, res.data])
+      .catch(err => console.log(err));
+  
+    }
+  },
+  created(){
+      axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err))
+      // fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      // .then(response => this.todo = response.data)
+      // .catch(err=> console.log(err))
   }
 }
 </script>
